c5b0d01122741338c9e2df8ede142d4e64a0e640
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2024-07-16 12:08:49 -0300

    criado logica de datas

--- src/main/java/br/com/registros/api/resources/CampanhasResources.java
+++ src/main/java/br/com/registros/api/resources/CampanhasResources.java
@@ -1,5 +1,9 @@
 package br.com.registros.api.resources;
 
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +21,32 @@ public class CampanhasResources {
 	
     //GET
     @GetMapping
-    public ResponseEntity<String> campanhas(@RequestParam(value = "numSerie", required = true) String numSerie,
+    public ResponseEntity<String> verificarDatas(@RequestParam(value = "numSerie", required = true) String numSerie,
     		@RequestParam(value = "numCupom", required = true) String numCupom) {
+    	
+    	Logger log = LoggerFactory.getLogger(CampanhasResources.class);
+    	
+        LocalDate dataAtual = LocalDate.now();
+        
+        LocalDate dataTeste = LocalDate.of(dataAtual.getYear(), 9, 1);
+        
+        // Cria uma data representando o primeiro dia de agosto deste ano
+        LocalDate dataAgosto = LocalDate.of(dataAtual.getYear(), 8, 1);
+        
+        // Cria uma data representando o primeiro dia de setembro deste ano
+        LocalDate dataSetembro = LocalDate.of(dataAtual.getYear(), 9, 1);
+        
+        if (dataAgosto.equals(dataTeste)) {
+        	log.info("Validar a range da série para Agosto");
+        }
+        
+        if (dataSetembro.equals(dataTeste)) {
+         	log.info("Validar a range da série para Setembro");
+        }
 
-        return ResponseEntity.ok("Ok Serie: " + numSerie + " Cupom: " + numCupom);
+        return ResponseEntity.ok("Ok - Mes de Agosto: " + dataAgosto.getMonthValue() + " Ano de Agosto: " + dataAgosto.getYear() 
+        	+ " Mes de Setembro: " + dataSetembro.getMonthValue() + " Ano de Setembro: " + dataSetembro.getYear()
+        	+ " Data Atual: " + dataAtual.getDayOfMonth() + "-" + dataAtual.getMonthValue() + "-" + dataAtual.getYear());
         
     }
 
